aa048bcae3b60f92ba24519167d02dbf197556c1
Paul Thomas <paulthomas2107@gmail.com>
2022-11-07 17:29:55 +0000

    More

--- getGameData.py
+++ getGameData.py
@@ -4,8 +4,9 @@ import shutil
 from subprocess import PIPE, run
 import sys
 
-
 GAME_DIR_PATTERN = "game"
+GAME_CODE_EXTENSION = ".go"
+GAME_COMPILE_COMMAND = ["go", "build"]
 
 
 def findAllGameDirsPaths(source):
@@ -50,6 +51,30 @@ def makeJSONMetaDataFile(path, game_dirs):
         json.dump(data, f)
 
 
+def compileGameCode(path):
+    code_file_name = None
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+                break
+        break
+
+    if code_file_name is None:
+        return
+
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+    runCommand(command, path)
+
+
+def runCommand(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print(result)
+    os.chdir(cwd)
+
+
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
@@ -63,6 +88,10 @@ def main(source, target):
     for src, dest in zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copyAndOverwrite(src, dest_path)
+        compileGameCode(dest_path)
+
+    json_path = os.path.join(target_path, "metadata.json")
+    makeJSONMetaDataFile(json_path, new_game_dirs)
 
 
 if __name__ == "__main__":
